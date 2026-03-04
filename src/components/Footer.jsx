24b89dc0c6e198439f11a728541b98463ea5844d
filipeasj <filipealves.profissional@gmail.com>
2026-03-04 09:56:31 -0300

    feat: Footer finalizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { resourcesLinks } from "../constants";
+import { communityLinks, platformLinks, resourcesLinks } from "../constants";
 
 const Footer = () => {
     return (
@@ -8,6 +8,26 @@ const Footer = () => {
                     <h3 className="text-md font-semibold mb-4">Resources</h3>
                     <ul className="space-y-2">
                         {resourcesLinks.map((link, index) => (
+                            <li key={index}>
+                                <a href={link.href}>{link.text}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <h3 className="text-md font-semibold mb-4">Platform</h3>
+                    <ul className="space-y-2">
+                        {platformLinks.map((link, index) => (
+                            <li key={index}>
+                                <a href={link.href}>{link.text}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <h3 className="text-md font-semibold mb-4">Community</h3>
+                    <ul className="space-y-2">
+                        {communityLinks.map((link, index) => (
                             <li key={index}>
                                 <a href={link.href}>{link.text}</a>
                             </li>
